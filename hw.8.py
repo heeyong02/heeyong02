c3ad6ae155d74ac91a1092d0b238969cfc046978
heeyong02 <152494017+heeyong02@users.noreply.github.com>
2024-05-14 11:13:01 +0900

    Add files via upload

---
+++ hw.8.py
@@ -0,0 +1,26 @@
+shopping_bag = []
+count_bag = []  #장바구니
+total = {}
+print('[구입]')
+while True:
+    item = input('상품명? ')
+    if item == '':
+       break
+    else: 
+       count = input('수량은? ')
+       print(f'장바구니에 {item} {count}개가 담겼습니다.\n')
+       shopping_bag.append(item)
+       count_bag.append(count)
+       total = dict(zip(shopping_bag, count_bag))
+print(f'\n>>> 장바구니 보기:{total}\n')
+print('[검색]')
+while True:
+    re_item = input('장바구니에서 확인하고자 하는 상품은? ')
+    if re_item == '':
+       print('구입 종료')
+       break
+    if re_item not in total:
+       print(f'장바구니에 {re_item}은(는) 없습니다.')
+    elif re_item in total:
+       print(f'{re_item}은(는) {total[re_item]}개 담겨 있습니다.')
+
